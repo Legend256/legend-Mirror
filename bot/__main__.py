19213ab17c737006d6f4be448a2b7e0d064ea707
Ash-The-King <110416898+Ash-The-King@users.noreply.github.com>
2022-08-01 22:58:04 +0530

    BAckup

--- bot/__main__.py
+++ bot/__main__.py
@@ -49,14 +49,12 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Leech/Mirror Group 🖤", "https://t.me/ashish_leech_group")
+    buttons.buildbutton("Owner ❤", "https://t.me/ashishsharmalegend")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-Welcome | {TITLE_NAME} service is ready for you
+Welcome | {TITLE_NAME} Hi, I am ready for you
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
